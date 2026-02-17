d4c9390e2bb1c8577369ade75d82ce261dc6d0a0
Vaibhav <vaibhav.pardeshi@atomberg.com>
2026-02-17 19:19:47 +0530

    Fix CORS to allow Netlify frontend

--- backend.py
+++ backend.py
@@ -10,10 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    allow_origins=["*"],  # Allows all origins (use only for testing!)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
